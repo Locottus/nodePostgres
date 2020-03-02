d345c9c2e9655a99c7babacf0b851662c5e4083d
herlich steven gonzalez zambrano <herlich@gmail.com>
2020-03-02 13:20:01 -0600

    fiexed api for new network config

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const db = require('./queries')
+const dbIOT = require('./queriesIOT')
 const port = 3000
+const APIROUTE = '/incyt/api';
 
 app.use(bodyParser.json())
 app.use(
@@ -11,15 +12,31 @@ app.use(
   })
 )
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+//metodo root
+app.get(APIROUTE, (request, response) => {
+  console.log('accessing route api');
+  response.json({ info: 'Node.js, Express, Nginx and Postgres API' })
 })
 
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+
+//METODOS PARA IOT VULCANOLOGIA
+app.get(APIROUTE + '/iotGet', () => {
+  console.log('entrando al get de IOT');
+  dbIOT.getUsers;
+});
+
+app.post(APIROUTE + '/iotPost', () => {
+  console.log('entrando al post de IOT');
+  dbIOT.createUser;
+});
+
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
@@ -29,5 +46,6 @@ app.listen(port, () => {
 // CREATE TABLE users (
 //     ID SERIAL PRIMARY KEY,
 //     name VARCHAR(30),
-//     email VARCHAR(30)
+//     email VARCHAR(30),
+//     unique(email)
 //   );
