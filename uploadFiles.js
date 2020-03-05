2a660b5ec42a3f9ea84825e26c7186216f228279
herlich steven gonzalez zambrano <herlich@gmail.com>
2020-03-05 14:37:29 -0600

    added database log for files

--- uploadFiles.js
+++ uploadFiles.js
@@ -1,3 +1,30 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: '172.17.250.12',
+  database: 'hashFiles',
+  password: 'postgres2020!Incyt',
+  port: 5432,
+})
+
+
+
+
+const grabaCatalogo = (request) => {
+  //pool.query('insert into fileCatalog (fieldname,originalname,encoding,mimetype,destination,filename,path,size)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [name, email], (error, results) => {
+const { fieldname,originalname,encoding,mimetype,destination,filename,path,size } = request.file;
+  console.log('esto es un post ' + fieldname + ' ' + originalname + ' ' + encoding + ' '+ mimetype + ' '+ destination + ' '+ filename + ' '+ path + ' '+ size );
+  
+pool.query('insert into fileCatalog (fieldname,originalname,encoding,mimetype,destination,filename,path,size)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
+[fieldname,originalname,encoding,mimetype,destination,filename,path,size], (error, results) => {
+  if (error) {
+    throw error
+  }
+  //response.status(201).send(`User added with ID: ${results.body}`);
+  //response.status(201).send(`{'msg':'OK'}`);
+  console.log("no hubo error");
+})
+}
 
 
 
@@ -11,8 +38,11 @@ const getFile = (request, response) => {
 
 const postFile = (request, response) => {
   console.log(request.file);
+  grabaCatalogo(request);
   response.status(201).send(request.file);
 }
+
+
 
 
 
