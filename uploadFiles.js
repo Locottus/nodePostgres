c0601e824768cfb72643f6fd40afb464d5ad4468
herlich steven gonzalez zambrano <herlich@gmail.com>
2020-03-12 15:06:10 -0600

    updated services hash files and iot services to work with array of objects

--- uploadFiles.js
+++ uploadFiles.js
@@ -10,20 +10,32 @@ const pool = new Pool({
 
 
 
- const grabaCatalogo = (request) => {
+const grabaCatalogo = (request) => {
   //pool.query('insert into fileCatalog (fieldname,originalname,encoding,mimetype,destination,filename,path,size)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [name, email], (error, results) => {
-const { fieldname,originalname,encoding,mimetype,destination,filename,path,size } = request.file;
-  console.log('esto es un post ' + fieldname + ' ' + originalname + ' ' + encoding + ' '+ mimetype + ' '+ destination + ' '+ filename + ' '+ path + ' '+ size );
-  
-pool.query('insert into fileCatalog (fieldname,originalname,encoding,mimetype,destination,filename,path,size)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
-[fieldname,originalname,encoding,mimetype,destination,filename,path,size], (error, results) => {
-  if (error) {
-    throw error
-  }
-  //response.status(201).send(`User added with ID: ${results.body}`);
-  //response.status(201).send(`{'msg':'OK'}`);
-  console.log("no hubo error");
-})
+  const { fieldname, originalname, encoding, mimetype, destination, filename, path, size } = request.file;
+  const origen = request.query.origen;
+  var i = originalname.indexOf('.');
+  var ext = originalname.substring(i, originalname.length);
+  var webName = filename + ext;
+  //console.log('esto es un post ' + fieldname + ' ' + originalname + ' ' + encoding + ' '+ mimetype + ' '+ destination + ' '+ filename + ' '+ path + ' '+ size );
+  //console.log('origen del archivo: ' + origen);
+  pool.query('insert into fileCatalog (fieldname,originalname,encoding,mimetype,destination,filename,path,size,origen,extension,webname)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,$11)',
+    [fieldname, originalname, encoding, mimetype, destination, filename, 'https://arcgis-web.url.edu.gt/incyt/api/HashFiles/uploads/' + webName, size, origen, ext, webName], (error, results) => {
+      if (error) {
+        throw error
+      }
+      //response.status(201).send(`User added with ID: ${results.body}`);
+      //response.status(201).send(`{'msg':'OK'}`);
+      //console.log("no hubo error");
+    })
+
+
+    //renombramos el archivo para que tenga extension
+    var fs = require('fs');
+    fs.rename('uploads/' + filename, 'uploads/' + webName , function(err) {
+        if ( err ) console.log('ERROR: ' + err);
+    });
+
 }
 
 
